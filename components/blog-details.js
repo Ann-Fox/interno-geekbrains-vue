457566c9d93bc1d79eb415aa257648e911ef2d1f
Ann-Fox <fx8565@mail.ru>
2023-08-29 18:47:38 +0500

    no components(

---
+++ components/blog-details.js
@@ -0,0 +1,73 @@
+const compItem = {
+    data() {
+        return {
+            // details: [
+            //     {
+            //         id: 1,
+            //         titleOne: 'Let`s Get Solution for Building                  Construction Work',
+            //         imgOne: 'images/blog-datails-kitchen-1.jpg',
+            //         date: '26 December,2022',
+            //         pOne: 'Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don`t look even slightly believable',
+            //         titleTwo: 'Design sprints are great',
+            //         pTwo: 'Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.',
+            //         imgTwo: 'images/blog-datails-kitchen-2.jpg',
+            //                       }
+            // ],
+        }
+    },
+    methods: {
+       
+    },
+    template: `
+   `,
+}
+
+Vue.component('BlogDetails', {
+    template: `<!-- <div class="blog__news">
+    <div class="blog__news__item">
+       <h1>Let's Get Solution for Building
+           Construction Work</h1>
+       <img src="images/blog-datails-kitchen-1.jpg" alt="">
+       <div class="blog__item__date">
+           <p>26 December,2022 </p>
+           <ul>
+               <li><a href="#">Interior</a></li>
+               <li><a href="">Home</a></li>
+               <li><a href="#">Decore</a></li>
+           </ul>
+       </div>
+       <p>Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to
+           popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have
+           suffered alteration in some form, by injecthumour, or randomised words which don't look even
+           slightly believable. </p>
+       <p>Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to
+           repeat predefined chunks as necessary.</p>
+   </div>
+   <div>
+       <img src="images/blog-datails-quotes.png" alt="quotes">
+       <h3>The details are not the details. They make the design.</h3>
+   </div>
+
+   <div class="blog__news__item">
+       <h1>Design sprints are great</h1>
+       <p>Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to
+           popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have
+           suffered. </p>
+       <ul>
+           <li>Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but
+               the majority have suffered.</li>
+           <li>Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but
+               the majority have suffered.</li>
+           <li>Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but
+               the majority have suffered.</li>
+       </ul>
+       <img src="images/blog-datails-kitchen-2.jpg" alt="">
+
+
+       <p>Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to
+           popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have
+           suffered.</p>
+   </div>
+</div>`,
+});
+
